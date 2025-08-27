8cd22d4702ac3bc34e9c31bc4ee1d2f9e14f1063
tkuschi <153780789+tkuschi@users.noreply.github.com>
2025-08-27 13:07:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
 
+  // Timeline für Header
+  gsap.to(".header-inner", {
+    paddingTop: "4rem",   // Zielwerte, anpassen wie du willst
+    paddingBottom: "4rem",
+    duration: 0.5,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: "body",        // irgendwas, das sicher beim Scrollen bewegt wird
+      start: "top top+=1",    // löst quasi sofort beim ersten Scroll-Pixel aus
+      toggleActions: "play none none none",
+      once: true              // nur einmal abspielen
+    }
+  });
