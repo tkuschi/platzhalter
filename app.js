8a128a3479900c5995f80d65ecb99c5a1b69f3c8
tkuschi <153780789+tkuschi@users.noreply.github.com>
2025-08-27 13:19:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 console.log("JS geladen 🎉");
 
-// Timeline für Header
-  gsap.to(".header-inner", {
-    paddingLeft: "12rem",   // Zielwerte, anpassen wie du willst
-    paddingRight: "12rem",
-    duration: 0.5,
-    ease: "power2.out",
-    scrollTrigger: {
-      trigger: "body",        // irgendwas, das sicher beim Scrollen bewegt wird
-      start: "top top+=1",    // löst quasi sofort beim ersten Scroll-Pixel aus
-      toggleActions: "play none none none",
-      once: true              // nur einmal abspielen
-    }
-  });
+gsap.to(".header-inner", {
+  paddingLeft: "12rem",
+  paddingRight: "12rem",
+  duration: 0.8,         // knapp unter 1 Sekunde
+  ease: "power4.out",    // knackiger, schneller Start, smoothes Ende
+  scrollTrigger: {
+    trigger: "body",
+    start: "top top+=1", 
+    toggleActions: "play none none none",
+    once: true
+  }
+});
